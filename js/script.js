4482e63b73b29121ef837b8a923b9492e930d923
jyothishpg <jyothishpg.pg@gmail.com>
2024-11-26 12:20:09 +0530

    style, script, html, multi select

--- js/script.js
+++ js/script.js
@@ -107,3 +107,23 @@ window.onload = function(){
 $(".ticketsResolution").click(function() {
   window.location.href = 'tickets-resolution.html'
 });
+if ($(".dropdown-mul-1").length) {
+var Random = Mock.Random;
+var json1 = Mock.mock({
+  "data|10-50": [{
+    name: function() {
+      return Random.name(true)
+    },
+    "id|+1": 1,
+    "disabled|1-2": true,
+    groupName: 'Group Name',
+    "groupId|1-4": 1,
+    "selected": false
+  }]
+});
+$('.dropdown-mul-1').dropdown({
+  data: json1.data,
+  limitCount: 40,
+  multipleMode: 'label',
+});
+}
